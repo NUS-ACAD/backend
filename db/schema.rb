9475baad53ef332f7a3c267198378098f6d863c2
zhuoyang125 <zhuoyang125@gmail.com>
2022-01-07 15:51:03 +0800

    add some seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_052223) do
+ActiveRecord::Schema.define(version: 2022_01_07_072644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2022_01_07_052223) do
     t.integer "order"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "module_title", null: false
     t.index ["semester_id", "module_code"], name: "index_mods_on_semester_id_and_module_code", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+feeds =  Feed.create([
+    {
+        user_id: 1,
+        activity_type: "created a new plan",
+        plan_id: 1,
+        second_plan_id: nil,
+        group_id: nil,
+        raw_data: nil
+    },
+    {
+        user_id: 2,
+        activity_type: "created a new plan",
+        plan_id: 2,
+        second_plan_id: nil,
+        group_id: nil,
+        raw_data: nil
+    },
+])
+
