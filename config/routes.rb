6555f8648e498a868063044729e1f3a5dee7c03d
zhuoyang125 <zhuoyang125@gmail.com>
2022-01-07 16:15:19 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :feeds, :follows, :invites, :likes, :members, :mods, :plans, :semesters, :users
+  resources :feeds, :follows, :invites, :likes, :members, :mods, :plans, :semesters, :users, :groups
 end
